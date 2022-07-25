30a1ce7dbab363f513f5f65cc400ee643cc69342
Nora von Thenen <nora.von.thenen@sap.com>
2022-07-25 14:47:55 +0200

    python app - submission week 2

---
+++ python-app/helloworld.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Welcome (Resource):
+
+    def get(self):
+        return 'Hello World!'
+
+
+api.add_resource(Welcome, '/')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', '3333')
